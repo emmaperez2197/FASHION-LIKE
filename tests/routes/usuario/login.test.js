1f76b989043bc25792c45eef037317aa0e8d9113
marcosgabriel2297 <mvera@moodtechnology.com.ar>
2022-06-27 12:53:55 -0300

    se creo el archivo para hacer los test del ep login

---
+++ tests/routes/usuario/login.test.js
@@ -0,0 +1,20 @@
+const sandbox = require('sinon').createSandbox();
+const assert = require('assert');
+
+const { mockResponse, mockRequest } = require('../../mocks/index');
+
+describe('Login Usuario api test', () => {
+	afterEach(() => sandbox.restore());
+
+	context('When errors do not ocurrs', () => {
+
+	});
+
+	context('When an errors ocurrs', () => {
+
+	});
+
+	context('When database errors occur', () => {
+
+	});
+});
